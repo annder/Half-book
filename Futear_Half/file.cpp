9725f64a70e88349125c796b3af2503349dde115
annder <2461067798@qq.com>
2018-06-22 16:46:13 +0800

    ..

--- Futear_Half/file.cpp
+++ Futear_Half/file.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 char print_line() {
 	FILE *fp = fopen("hello.txt", "r");
@@ -24,3 +25,46 @@ void write_file_char(char *filename,char word) {
 	fputc(word, pf);
 	fclose(pf);
 };
+
+void delete_file(char *file_name) {
+	remove(file_name);
+}
+
+void rename_file (char *file_name,char *change_name){
+	rename(file_name,change_name);
+}
+
+
+///////
+///主要是退回到流中///
+int read_int() {
+	int value;
+	int ch;
+	value = 0;
+	while ((ch = getchar())!= EOF && isdigit(ch)) {
+		value *= 10;
+		value += ch - '0';
+	}
+	ungetc(ch, stdin);
+	return value;
+}
+
+#define MAX_LINE_LENGHT 1024
+void copyline(FILE *input, FILE *output) {
+	char buffer[MAX_LINE_LENGHT];
+	// MAX_LINE_LENGHT就是读取buffer的内存
+	while (fgets(buffer,MAX_LINE_LENGHT,input) != NULL)
+	{
+		fputs(buffer, output);
+	}
+};
+
+//Then, i write a function to sever a string.
+
+char *get_string_file(FILE* filename) {
+	#define  MAX_LENGHT 1024	
+	char result[MAX_LENGHT];
+	while ((fgets(result, MAX_LINE_LENGHT, filename))!= NULL);
+	return result;
+};
+
--- Futear_Half/main.cpp
+++ Futear_Half/main.cpp
@@ -1,11 +1,19 @@
-#include "stdio.h"
+#include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 extern char print_line();
 extern void print_all(char *filename,char *mode);
 extern void write_file_char(char *filename, char word);
+extern void delete_file(char *filename);
+extern void rename_file(char *file_name, char *change_name);
+extern void copyline(FILE *input, FILE *output);
+extern char *get_string_file(FILE* filename);
 
 int main() {
-	write_file_char("text.txt", 'Y');
-	print_all("text.txt","r+");
+	char o[12];
+	FILE *fp = fopen("c.txt","w+");
+	fscanf(fp, "%s", o);
+	printf("%d\n", o);
+	fclose(fp);
 };
